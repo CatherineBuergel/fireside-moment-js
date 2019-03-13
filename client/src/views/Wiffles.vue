f73975a81900fe25dde0bfac4d062a9bf2e093e5
porterwilcox <wilcox.porter@gmail.com>
2019-03-12 18:18:54 -0600

    in-component route guard

--- client/src/views/Wiffles.vue
+++ client/src/views/Wiffles.vue
@@ -1,6 +1,15 @@
 <template>
    <div class="wiffles container-fluid">
-      <wiffle-form />
+      <div class="row">
+         <div class="col-10 offset-1">
+            <h5 class="action" @click="create = !create">{{!create ? 'Post a Wiffle' : 'Cancel'}}</h5>
+            <form @submit.prevent="post" v-if="create" class="d-flex flex-column">
+                <input placeholder="What's your Name?" type="text" required v-model="wiffle.creator">
+                <textarea required placeholder="What I Feel Like Expressing..." v-model="wiffle.content" cols="20" rows="5"></textarea>
+                <button type="submit" class="btn btn-info">Submit</button>
+            </form>
+         </div>
+      </div>
       <div class="row">
          <div v-for="wiffle in wiffles" :key="wiffle.id" class="col-4">
             <div class="card">
@@ -19,12 +28,25 @@
 
 <script>
 import Moment from 'moment'
-import WiffleForm from '@/components/WiffleForm.vue'
 export default {
    name: "wiffles",
+   //in-component route guards should only be used on views and not components
+   beforeRouteLeave(to, from, next){
+      if (this.wiffle.creator) {
+         let leave = confirm("Are you sure you want to leave? You have unfinished business.")
+         if (!leave) return next(false)
+      }
+      next()
+   },
    props: [],
    data() {
-      return {}
+      return {
+         create: false,
+          wiffle: {
+              creator: '',
+              content: ''
+          }
+      }
    },
    computed: {
       wiffles() {
@@ -36,9 +58,13 @@ export default {
          return Moment(String(date)).fromNow()
       }
    },
-   methods: {},
-   components: {
-      WiffleForm
-   }
+   methods: {
+      post() {
+           this.$store.dispatch('postWiffle', this.wiffle)
+           this.create = false
+           this.wiffle = {creator: '', content: ''}
+       }
+   },
+   components: { }
 }
 </script>
